bc4904b1234d51b2c878bea86050f9ec434efc63
Vladimir Rech <rechvladimir@hotmail.com>
2020-09-27 21:21:29 -0300

    Adding protuguese phrases

--- chatt.py
+++ chatt.py
@@ -12,18 +12,28 @@ bot = ChatBot(
     )
     
 trainer = ListTrainer(bot)
+#trainer.train([
+#    'Hi',
+#    'Hello',
+#    'I need your assistance regarding my order',
+#    'Please, Provide me with your order id',
+#    'I have a complaint.',
+#    'Please elaborate, your concern',
+#    'How long it will take to receive an order ?',
+#    'An order takes 3-5 Business days to get delivered.',
+#    'Okay Thanks',
+#    'No Problem! Have a Good Day!'
+#    ])
+
 trainer.train([
-    'Hi',
-    'Hello',
-    'I need your assistance regarding my order',
-    'Please, Provide me with your order id',
-    'I have a complaint.',
-    'Please elaborate, your concern',
-    'How long it will take to receive an order ?',
-    'An order takes 3-5 Business days to get delivered.',
-    'Okay Thanks',
-    'No Problem! Have a Good Day!'
-    ])
+    'Oi',
+    'Olá',
+    'Bom dia',
+    'Bom dia como você está?',
+    'Eu estou bem obrigado',
+    'Como eu posso ajudar?',
+    'Bye',
+    'Até mais!'])
     
 name = input("Enter Your Name: ")
 print("Welcome to the Bot Service! Let me know how can I help you?")
